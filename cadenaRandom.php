89ed6229a091e1504d99ca1669e9759cf09a1ede
burngeek8 <burngeek8@gmail.com>
2015-06-11 11:07:00 -0500

    rescatado

---
+++ cadenaRandom.php
@@ -0,0 +1,33 @@
+<?php
+function RandomString($length=6,$uc=TRUE,$n=TRUE,$sc=FALSE)
+{
+    $source = 'abcdefghijklmnopqrstuvwxyz';
+    if($uc==1) $source .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if($n==1) $source .= '1234567890';
+    if($sc==1) $source .= '|@#~$%()=^*+[]{}-_';
+    if($length>0){
+        $rstr = "";
+        $source = str_split($source,1);
+        for($i=1; $i<=$length; $i++){
+            mt_srand((double)microtime() * 1000000);
+            $num = mt_rand(1,count($source));
+            $rstr .= $source[$num-1];
+        }
+ 
+    }
+    return $rstr;
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cadena Random</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta name="HandheldFriendly" content="true" />
+</head>
+<body>
+    <p style="text-transform: uppercase;"> <?= RandomString(); ?> </p>
+</body>
+</html>
